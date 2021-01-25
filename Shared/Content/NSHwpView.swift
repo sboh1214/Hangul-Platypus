2a3793cc17015eec5049c93d9378776a96427e8d
Seungbin Oh <sboh1214@gmail.com>
2021-01-26 00:19:24 +0900

    Sync

--- Shared/Content/NSHwpView.swift
+++ Shared/Content/NSHwpView.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import CoreHwp
 
 struct NSHwpView: NSViewRepresentable {
     typealias NSViewType = NSTextView
+    
+    var hwpFile: HwpFile
 
     func makeNSView(context: Context) -> NSViewType {
         let view = NSTextView()
+        hwpFile.sectionArray[0].paragraph[0].paraText?.charArray
         return view
     }
 
@@ -15,6 +19,6 @@ struct NSHwpView: NSViewRepresentable {
 
 struct NSHwpViewPreviews: PreviewProvider {
     static var previews: some View {
-        NSHwpView()
+        NSHwpView(hwpFile: HwpFile())
     }
 }
