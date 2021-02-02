e9878fcd03d8f22ffaf193decbf0a2f07e9d188f
Seungbin Oh <sboh1214@gmail.com>
2021-02-03 03:11:54 +0900

    Refactor

--- Shared/Content/NSHwpView.swift
+++ Shared/Content/NSHwpView.swift
@@ -1,6 +1,64 @@
 import SwiftUI
 import CoreHwp
 
+func makeRange(from startingIndex: [UInt32], endAt endIndex: Int) -> [Range<Int>] {
+    var rangeArray = [Range<Int>]()
+    var index = 0
+    while true {
+        if index == startingIndex.count - 1 {
+            let startIndex = Int(startingIndex[index])
+            rangeArray.append(startIndex..<endIndex)
+        } else {
+            let startIndex = Int(startingIndex[index])
+            let endIndex = Int(startingIndex[index + 1])
+            rangeArray.append(startIndex..<endIndex)
+        }
+
+        if index < startingIndex.count - 1 {
+            index += 1
+        } else {
+            break
+        }
+    }
+    return rangeArray
+}
+
+func makeString(from charArray: ArraySlice<HwpChar>) -> String {
+    let characterArray = charArray.compactMap { char -> Character? in
+        if char.type == .char {
+            return Character(UnicodeScalar(char.value)!)
+        } else {
+            return Character(UnicodeScalar(UInt16(32))!) // Space
+        }
+    }
+    return String(characterArray)
+}
+
+func makeFont(from attribute: HwpCharShape) -> NSFont? {
+    let family = "HCR Batang"
+
+    let traits: NSFontTraitMask
+    if attribute.property.isBold && attribute.property.isItalic {
+        traits = [.boldFontMask, .italicFontMask]
+    } else if attribute.property.isBold {
+        traits = [.boldFontMask, .unitalicFontMask]
+    } else if attribute.property.isItalic {
+        traits = [.unboldFontMask, .italicFontMask]
+    } else {
+        traits = [.unboldFontMask, .unitalicFontMask]
+    }
+
+    let size = CGFloat(attribute.baseSize) * CGFloat(attribute.faceRelativeSize[0]) / CGFloat(10000)
+
+    let fontManager = NSFontManager.shared
+    guard let font = fontManager.font(withFamily: family, traits: traits, weight: 0, size: size)
+    else {
+        print("no exist")
+        return NSFont.systemFont(ofSize: size)
+    }
+    return font
+}
+
 struct NSHwpView: NSViewRepresentable {
     typealias NSViewType = NSTextView
 
@@ -22,44 +80,22 @@ struct NSHwpView: NSViewRepresentable {
                     return
                 }
 
-                var index = 0
-                while true {
-                    let rangeCharArray: ArraySlice<HwpChar>
-                    if index == paragraph.paraCharShape.startingIndex.count - 1 {
-                        let startIndex = Int(paragraph.paraCharShape.startingIndex[index])
-                        rangeCharArray = charArray[startIndex..<charArray.endIndex]
-                    } else {
-                        let startIndex = Int(paragraph.paraCharShape.startingIndex[index])
-                        let endIndex = Int(paragraph.paraCharShape.startingIndex[index + 1])
-                        rangeCharArray = charArray[startIndex..<endIndex]
-                    }
+                let rangeArray = makeRange(from: paragraph.paraCharShape.startingIndex, endAt: charArray.endIndex)
 
-                    let characterArray = rangeCharArray.compactMap { char -> Character? in
-                        if char.type == .char {
-                            return Character(UnicodeScalar(char.value)!)
-                        } else {
-                            return Character(UnicodeScalar(UInt16(32))!) // Space
-                        }
-                    }
+                rangeArray.enumerated().forEach { (index, range) in
 
-                    let string = String(characterArray)
-                    let attributedString = NSMutableAttributedString(string: string)
-                    let range = (string as NSString).range(of: string)
+                    let string = makeString(from: charArray[range])
 
                     let attribute = hwp.docInfo.idMappings.charShapeArray[Int(paragraph.paraCharShape.shapeId[index])]
 
-                    if attribute.property.isBold {
-                        let font = NSFont.boldSystemFont(ofSize: 16)
-                        attributedString.addAttribute(.font, value: font, range: range)
-                    }
+                    let attributes: [NSAttributedString.Key: Any] = [
+                        .font: makeFont(from: attribute),
+                        .foregroundColor: attribute.faceColor.nsColor
+                    ]
+
+                    let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
 
                     allString.append(attributedString)
-
-                    if index < paragraph.paraCharShape.startingIndex.count - 1 {
-                        index += 1
-                    } else {
-                        break
-                    }
                 }
             }
         }
--- Shared/Utils/HwpColor+Extension.swift
+++ Shared/Utils/HwpColor+Extension.swift
@@ -1,8 +1,47 @@
-//
-//  HwpColor+Extension.swift
-//  Like Hangul
-//
-//  Created by 오승빈 on 2021/02/03.
-//
-
+import CoreHwp
 import Foundation
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
+extension HwpColor {
+    var cgColor: CGColor {
+        get {
+            let red = CGFloat(self.red) / CGFloat(255)
+            let green = CGFloat(self.green) / CGFloat(255)
+            let blue = CGFloat(self.blue) / CGFloat(255)
+            let alpha = CGFloat(1)
+            return CGColor(red: red, green: green, blue: blue, alpha: alpha)
+        }
+    }
+
+    #if os(iOS)
+    var uiColor: UIColor {
+        get {
+            return UIColor(cgColor: self.cgColor)
+        }
+    }
+    #endif
+
+    #if os(macOS)
+    var nsColor: NSColor {
+        get {
+            return NSColor(cgColor: self.cgColor)!
+        }
+    }
+    #endif
+
+    #if canImport(SwiftUI)
+    var color: Color {
+        get {
+            return Color(self.cgColor)
+        }
+    }
+    #endif
+}
