5391c7c32040f793457210e3d0627b452e05d328
Seungbin Oh <sboh1214@gmail.com>
2021-02-03 03:18:08 +0900

    Fix Lint

--- Shared/Utils/HwpColor+Extension.swift
+++ Shared/Utils/HwpColor+Extension.swift
@@ -12,36 +12,28 @@ import SwiftUI
 
 extension HwpColor {
     var cgColor: CGColor {
-        get {
-            let red = CGFloat(self.red) / CGFloat(255)
-            let green = CGFloat(self.green) / CGFloat(255)
-            let blue = CGFloat(self.blue) / CGFloat(255)
-            let alpha = CGFloat(1)
-            return CGColor(red: red, green: green, blue: blue, alpha: alpha)
-        }
+        let red = CGFloat(self.red) / CGFloat(255)
+        let green = CGFloat(self.green) / CGFloat(255)
+        let blue = CGFloat(self.blue) / CGFloat(255)
+        let alpha = CGFloat(1)
+        return CGColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 
     #if os(iOS)
     var uiColor: UIColor {
-        get {
-            return UIColor(cgColor: self.cgColor)
-        }
+        return UIColor(cgColor: self.cgColor)
     }
     #endif
 
     #if os(macOS)
     var nsColor: NSColor {
-        get {
-            return NSColor(cgColor: self.cgColor)!
-        }
+        return NSColor(cgColor: self.cgColor)!
     }
     #endif
 
     #if canImport(SwiftUI)
     var color: Color {
-        get {
-            return Color(self.cgColor)
-        }
+        return Color(self.cgColor)
     }
     #endif
 }
