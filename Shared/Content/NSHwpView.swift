c2758656f858d5f1832db8bcef0a97bc20ac4a3b
Seungbin Oh <sboh1214@gmail.com>
2021-08-18 23:44:48 +0900

    Add supplemental config on NSHwpView

--- Shared/Content/NSHwpView.swift
+++ Shared/Content/NSHwpView.swift
@@ -6,26 +6,34 @@ struct NSHwpView: NSViewRepresentable {
 
     @EnvironmentObject var file: FileObject
 
-    var textViewManager: TextViewManager!
+    @State var textViewManager: TextViewManager!
 
     func makeNSView(context: Context) -> NSViewType {
         // context.environment.colorScheme == .light
-        let hwp = file.document.wrappedValue.hwp
-        let textViewManager = TextViewManager(from: hwp)
 
         let scrollView = NSScrollView()
+        let contentSize = scrollView.contentSize
         scrollView.borderType = .noBorder
         scrollView.hasVerticalScroller = true
         scrollView.hasHorizontalScroller = true
         scrollView.hasHorizontalRuler = true
-
-        let clipView = NSClipView()
-        scrollView.contentView = clipView
-
+        
+        let hwp = file.document.wrappedValue.hwp
+        let textViewManager = TextViewManager(from: hwp)
+        
         let textView = NSTextView(frame: NSRect(x: 0, y: 0, width: 1000, height: 2000), textContainer: textViewManager.textContainer)
         textView.isEditable = false
         textView.isRichText = true
         textView.isSelectable = true
+        textView.breakUndoCoalescing()
+        
+        textView.minSize = CGSize(width: 0, height: 0)
+        textView.maxSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+        textView.isVerticallyResizable = true
+        textView.isHorizontallyResizable = true
+        textView.frame = CGRect(x: scrollView.frame.origin.x, y: scrollView.frame.origin.y, width: contentSize.width, height: contentSize.height)
+        textView.autoresizingMask = [.width, .height]
+        
         scrollView.documentView = textView
 
         return scrollView
