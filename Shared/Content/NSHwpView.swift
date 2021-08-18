ebaafb3351b8d0ba1550b9a22acc7a2359e31814
Seungbin Oh <sboh1214@gmail.com>
2021-08-19 00:10:33 +0900

    Remove Trailing Whitespace

--- Shared/Content/NSHwpView.swift
+++ Shared/Content/NSHwpView.swift
@@ -17,23 +17,23 @@ struct NSHwpView: NSViewRepresentable {
         scrollView.hasVerticalScroller = true
         scrollView.hasHorizontalScroller = true
         scrollView.hasHorizontalRuler = true
-        
+
         let hwp = file.document.wrappedValue.hwp
         let textViewManager = TextViewManager(from: hwp)
-        
+
         let textView = NSTextView(frame: NSRect(x: 0, y: 0, width: 1000, height: 2000), textContainer: textViewManager.textContainer)
         textView.isEditable = false
         textView.isRichText = true
         textView.isSelectable = true
         textView.breakUndoCoalescing()
-        
+
         textView.minSize = CGSize(width: 0, height: 0)
         textView.maxSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
         textView.isVerticallyResizable = true
         textView.isHorizontallyResizable = true
         textView.frame = CGRect(x: scrollView.frame.origin.x, y: scrollView.frame.origin.y, width: contentSize.width, height: contentSize.height)
         textView.autoresizingMask = [.width, .height]
-        
+
         scrollView.documentView = textView
 
         return scrollView
